2f5e5284a74bdea504cfe2565575a8355b17b2df
Nick Soan <nick.soan@googlemail.com>
2020-07-20 23:10:04 +0100

    Add project files.

---
+++ GameTest/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GameTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ground1 = new System.Windows.Forms.PictureBox();
+            this.ground2 = new System.Windows.Forms.PictureBox();
+            this.pauseMenu = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pauseMenu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 1;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.player.Location = new System.Drawing.Point(100, 498);
+            this.player.Margin = new System.Windows.Forms.Padding(0);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ground1
+            // 
+            this.ground1.AccessibleDescription = "ground";
+            this.ground1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ground1.Location = new System.Drawing.Point(-11, 563);
+            this.ground1.Margin = new System.Windows.Forms.Padding(0);
+            this.ground1.Name = "ground1";
+            this.ground1.Size = new System.Drawing.Size(473, 50);
+            this.ground1.TabIndex = 1;
+            this.ground1.TabStop = false;
+            // 
+            // ground2
+            // 
+            this.ground2.AccessibleDescription = "ground";
+            this.ground2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ground2.Location = new System.Drawing.Point(483, 563);
+            this.ground2.Margin = new System.Windows.Forms.Padding(0);
+            this.ground2.Name = "ground2";
+            this.ground2.Size = new System.Drawing.Size(1146, 50);
+            this.ground2.TabIndex = 2;
+            this.ground2.TabStop = false;
+            // 
+            // pauseMenu
+            // 
+            this.pauseMenu.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pauseMenu.Location = new System.Drawing.Point(483, 49);
+            this.pauseMenu.Name = "pauseMenu";
+            this.pauseMenu.Size = new System.Drawing.Size(476, 349);
+            this.pauseMenu.TabIndex = 3;
+            this.pauseMenu.TabStop = false;
+            this.pauseMenu.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1550, 820);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.ground2);
+            this.Controls.Add(this.ground1);
+            this.Controls.Add(this.pauseMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pauseMenu)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ground1;
+        private System.Windows.Forms.PictureBox ground2;
+        private System.Windows.Forms.PictureBox pauseMenu;
+    }
+}
+
---
+++ GameTest/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameTest
+{
+    public partial class Form1 : Form
+    {
+        private int startX = 100;
+        private int startY = 298;
+
+        private int gravity = 10;
+        private int gravityConst = 1;
+        private double jumpHeight = 100;
+        private int sideMoveSpeed = 3;
+        private int globalMoveSpeed = 3;
+        private int startingSideMoveSpeed = 3;
+        private double jumpPositionY = 1;
+        bool isPaused = false;
+        double cumulativeGravity = 0;
+        int gravityPerFrame = 3;
+
+        int screenHeight = 100;
+        int screenWidth = 100;
+        int screenRightMargin = 100;
+        int screenLeftMargin = 100;
+        private bool inJump;
+        private bool hasJumped;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Reset();
+
+            Screen myScreen = Screen.FromControl(this);
+            screenHeight = this.Height;
+            screenWidth = this.Width;
+            screenRightMargin = screenWidth - (screenWidth / 100 * 10);
+            screenLeftMargin = (screenWidth / 100 * 2);
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (!isPaused)
+            {
+                pauseMenu.Visible = false;
+                Move();
+                CheckJump();
+                MoveBackGround();
+                ApplyGravity();
+                CheckIsDead();
+            }
+            else
+            {
+                pauseMenu.Visible = true;
+            }
+        }
+
+        private void MoveBackGround()
+        {
+            var groundObjects = GetGroundObjects();
+
+            for (int i = 0; i < groundObjects.Count(); i++)
+            {
+                Color Warn1Color = Color.FromArgb(255, 0, 0);//default colour
+                if (groundObjects[i].Right < 0)
+                {
+                    groundObjects[i].Left = this.Width - 100;
+                    //this.Controls.Remove(groundObjects[i]);
+                    //PictureBox newGround = new PictureBox()
+                    //{
+                    //    Name = "ground" + new Guid(),
+                    //    Location = new Point(300, 563),
+                    //    Width = 1000,
+                    //    Height = 50,
+                    //    BackColor = Warn1Color,
+                    //    AccessibleDescription = "ground"
+                    //};
+                    //this.Controls.Add(newGround);
+
+
+                }
+
+                groundObjects[i].Left -= globalMoveSpeed;
+            }
+
+        }
+
+        private void ApplyGravity()
+        {
+            if (!IsOnGround())
+            {
+                cumulativeGravity += gravity;
+                while (cumulativeGravity != 0)
+                {
+                    cumulativeGravity -= gravityPerFrame;
+                    player.Top += gravityPerFrame;
+                    if (cumulativeGravity < 0)
+                    {
+                        cumulativeGravity = 0;
+                    }
+                }
+            }
+        }
+
+        private void Move()
+        {
+            if (player.Left <= screenLeftMargin)
+            {
+                sideMoveSpeed = sideMoveSpeed * -1;
+                player.Left += sideMoveSpeed;
+            }
+            else if (player.Left <= screenRightMargin)
+            {
+                player.Left += sideMoveSpeed;
+            }
+            else
+            {
+                sideMoveSpeed = sideMoveSpeed * -1;
+                player.Left += sideMoveSpeed;
+            }
+
+
+        }
+
+        private List<Control> GetGroundObjects()
+        {
+            List<Control> groundObjects = new List<Control>();
+
+            List<Control> c = Controls.OfType<PictureBox>().Cast<Control>().ToList();
+            foreach (var control in c)
+            {
+                if (control.AccessibleDescription == "ground")
+                {
+                    groundObjects.Add(control);
+                }
+            }
+
+            return groundObjects;
+
+        }
+
+        private void CheckJump()
+        {
+            if (inJump)
+            {
+                while (player.Top >= jumpPositionY)
+                {
+                    player.Top -= 1;
+                    Move();
+                    if (player.Top <= jumpPositionY)
+                    {
+                        inJump = false;
+                    }
+                    player.Top -= gravity;
+                }
+            }
+        }
+
+        private bool IsOnGround()
+        {
+            List<Control> groundObjects = GetGroundObjects();
+            bool isOnGround = false;
+            foreach (var ground in groundObjects)
+            {
+                if (player.Bounds.IntersectsWith(ground.Bounds))
+                {
+                    hasJumped = false;
+                    isOnGround = true;
+                }
+            }
+
+            return isOnGround;
+        }
+
+        private void CheckIsDead()
+        {
+            if ((player.Right >= screenWidth) || (player.Top >= screenHeight))
+            {
+                End();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((!inJump) && (!hasJumped))
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    inJump = true;
+                    hasJumped = true;
+                    jumpPositionY = player.Bottom - jumpHeight;
+                }
+                if (e.KeyCode == Keys.R)
+                {
+                    Reset();
+                }
+                if (e.KeyCode == Keys.P)
+                {
+                    if (isPaused)
+                    {
+                        isPaused = false;
+                    }
+                    else
+                    {
+                        isPaused = true;
+                    }
+
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            gravity = gravityConst;
+        }
+
+        private void End()
+        {
+            gameTimer.Stop();
+            isPaused = true;
+            Reset();
+
+        }
+
+        private void Reset()
+        {
+            player.Left = startX;
+            player.Top = startY;
+            sideMoveSpeed = startingSideMoveSpeed;
+
+            gameTimer.Start();
+        }
+    }
+}
